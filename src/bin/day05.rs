0205db9dfcff77f840dd34796586e7e91aaa37c6
sergey-melnychuk <8093171+sergey-melnychuk@users.noreply.github.com>
2023-12-05 19:11:56 +0100

    done: day05: 2/2
    Inspired by: https://github.com/mmdoogie/adventofcode2023/blob/2685d8fe5b4815e9f1cec1c60ed0fd7ce98c35f4/aoc_2023_05.py

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -1,70 +1,51 @@
 use advent_of_code_2023::*;
 
-// cargo run --bin day05 < test.txt
-// cargo run --bin day05 < txt/day05.txt
-
 fn main() {
     let (seeds, mappings) = parse(&lines());
-
     println!("{}", part1(&seeds, &mappings)); // 579439039
-    println!("{}", part2(&seeds, &mappings)); // .
-                                              // 60287808 - too high
-                                              // 
+    println!("{}", part2(&seeds, &mappings)); // 7873084
 }
 
 fn part2(seeds: &[usize], mappings: &[Mapping]) -> usize {
     let fst = seeds.iter().step_by(2);
     let len = seeds.iter().skip(1).step_by(2);
+    let seeds = fst.cloned().zip(len.cloned()).collect::<Vec<_>>();
 
-    // naive/bruteforce approach:
-    /*
+    let p = seeds.iter().map(|(_, len)| *len).max().unwrap_or_default();
+    let p = 10usize.pow((p as f64 / 100.0).log10().ceil() as u32);
+    let mut step = p;
+
+    let (lo, hi, best, _) = seeds.iter().cloned()
+        .map(|(src, len)| {
+            let lo = src;
+            let hi = src + len;
+            let mut min = usize::MAX;
+            let mut pos = lo;
+            for seed in (lo..hi).step_by(step) {
+                let x = map(seed, mappings);
+                if x < min {
+                    min = x;
+                    pos = seed;
+                }
+            }
+            (lo, hi, pos, min)
+        })
+        .min_by_key(|x| x.3)
+        .unwrap();
+
     let mut min = usize::MAX;
-    for (src, len) in fst.zip(len) {
-        let lo = *src;
-        let hi = *src + *len;
-        for x in lo..hi {
-            min = map(x, mappings).min(min);
-        }
+    let mut pos = best;
+    while step > 1 {
+        let a = lo.max(pos - step);
+        let b = hi.min(pos + step);
+        step /= 10;
+
+        (pos, min) = (a..b).step_by(step)
+            .map(|x| (x, map(x, mappings)))
+            .min_by_key(|x| x.1)
+            .unwrap();
     }
     min
-    */
-
-    // blows up with number of ranges (40M+)
-    fst.zip(len)
-        .map(|(fst, len)| (*fst, fst + len))
-        .flat_map(|(lo, hi)| map_range(lo, hi, &mappings))
-        .map(|(lo, _)| lo)
-        .min()
-        .unwrap_or_default()
-}
-
-fn map_range(lo: usize, hi: usize, mappings: &[Mapping]) -> Vec<(usize, usize)> {
-    let mut ret = vec![(lo, hi)];
-    for m in mappings {
-        ret = merge(ret);
-        ret = ret.into_iter()
-            .flat_map(|(lo, hi)| m.map_range(lo, hi))
-            .collect();
-    }
-    ret
-}
-
-fn merge(mut ranges: Vec<(usize, usize)>) -> Vec<(usize, usize)> {
-    ranges.sort_by_key(|r| r.0);
-    let mut ret = Vec::new();
-    for (a, b) in ranges {
-        let last = ret.last().cloned();
-        if let Some((lo, hi)) = last {
-            if a <= hi {
-                *ret.last_mut().unwrap() = (lo, b.max(hi));
-            } else {
-                ret.push((a, b));
-            }
-        } else {
-            ret.push((a, b));
-        }
-    }
-    ret
 }
 
 fn part1(seeds: &[usize], mappings: &[Mapping]) -> usize {
@@ -95,15 +76,6 @@ impl Rule {
         }
     }
 
-    fn map_range(&self, lo: usize, hi: usize) -> Option<(usize, usize)> {
-        if lo >= self.src + self.len || hi <= self.src {
-            return None;
-        }
-        let lo = lo.max(self.src) - self.src + self.dst;
-        let hi = hi.min(self.src + self.len) - self.src + self.dst;
-        Some((lo, hi))
-    }
-
     fn hits(&self, x: usize) -> bool {
         let end = self.src + self.len;
         (self.src..end).contains(&x)
@@ -122,41 +94,6 @@ impl Mapping {
             .filter_map(|rule| rule.map(x))
             .next()
             .unwrap_or(x)
-    }
-
-    fn map_range(&self, lo: usize, hi: usize) -> Vec<(usize, usize)> {
-        let mapped = self.rules.iter()
-            .filter_map(|r| r.map_range(lo, hi))
-            .collect::<Vec<_>>();
-
-        let mut rules = self.rules.iter().cloned()
-            .filter(|r| r.map_range(lo, hi).is_some())
-            .collect::<Vec<_>>();
-        rules.sort_by_key(|r| r.src);
-
-        let mut head = if let Some(head) = rules.iter().find(|r| r.hits(lo)) {
-            head.src + head.len
-        } else {
-            lo
-        };
-        let tail = if let Some(head) = rules.iter().find(|r| r.hits(hi)) {
-            head.src
-        } else {
-            hi
-        };
-
-        let rules = self.rules.iter().cloned()
-            .filter(|r| !r.hits(lo) && !r.hits(hi))
-            .collect::<Vec<_>>();
-
-        let mut unmapped: Vec<(usize, usize)> = Vec::new();
-        for rule in rules {
-            unmapped.push((head, rule.src));
-            head = rule.src + rule.len;
-        }
-        unmapped.push((head, tail));
-
-        mapped.into_iter().chain(unmapped.into_iter()).collect()
     }
 }
 
