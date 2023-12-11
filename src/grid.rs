543fe70b8e6d0fecfc6f3579213d7021f9051570
sergey-melnychuk <8093171+sergey-melnychuk@users.noreply.github.com>
2023-12-11 10:15:59 +0100

    ads: add bfs & dijkstra for grid

--- src/grid.rs
+++ src/grid.rs
@@ -1,9 +1,13 @@
-use std::{collections::HashSet, fmt::Debug};
+use std::collections::{HashMap as Map, HashSet as Set, VecDeque as Seq};
+use std::fmt::Debug;
 
 /// Identity function.
 pub fn id<T>(x: T) -> T {
     x
 }
+
+/// Type alias for a cell positio (row, column)
+pub type Cell = (usize, usize);
 
 /// Generic dense grid implementation
 #[derive(Debug)]
@@ -21,7 +25,7 @@ impl<T: Debug + 'static> Grid<T> {
             panic!("grid is empty");
         }
 
-        let cols = lines.iter().map(|line| line.len()).collect::<HashSet<_>>();
+        let cols = lines.iter().map(|line| line.len()).collect::<Set<_>>();
         if cols.len() > 1 {
             panic!("grid rows have different lengths");
         }
@@ -44,19 +48,19 @@ impl<T: Debug + 'static> Grid<T> {
     }
 
     /// Get grid item at a given position
-    pub fn get(&self, pos: &(usize, usize)) -> Option<&T> {
+    pub fn get(&self, pos: &Cell) -> Option<&T> {
         let (row, col) = *pos;
         self.data.get(row).and_then(|row| row.get(col))
     }
 
     /// Get mutable reference to a grid item at a given position
-    pub fn get_mut(&mut self, pos: &(usize, usize)) -> Option<&mut T> {
+    pub fn get_mut(&mut self, pos: &Cell) -> Option<&mut T> {
         let (row, col) = *pos;
         self.data.get_mut(row).and_then(|row| row.get_mut(col))
     }
 
     /// Get adjacent positions to a given one
-    pub fn adj(&self, pos: &(usize, usize)) -> Vec<(usize, usize)> {
+    pub fn adj(&self, pos: &Cell) -> Vec<Cell> {
         let prow = pos.0 as isize;
         let pcol = pos.1 as isize;
         let mut ret = Vec::with_capacity(9);
@@ -83,7 +87,7 @@ impl<T: Debug + 'static> Grid<T> {
     }
 
     /// Find positions of grid cells that match a predicate
-    pub fn find(&self, f: impl Fn(&T) -> bool) -> Vec<(usize, usize)> {
+    pub fn find(&self, f: impl Fn(&T) -> bool) -> Vec<Cell> {
         let mut ret = Vec::new();
         for row in 0..self.rows {
             for col in 0..self.cols {
@@ -143,4 +147,45 @@ impl<'a, T: Debug + 'static> Iterator for GridIter<'a, T> {
 
         Some((row, col, val))
     }
+}
+
+/// BFS traversal of a grid
+pub fn bfs<T: Debug + 'static>(
+    grid: &Grid<T>,
+    from: &Cell,
+    mut f: impl FnMut(Cell, Cell),
+) {
+    let mut seen: Set<Cell> = Set::new();
+    let mut queue: Seq<Cell> = Seq::new();
+    queue.push_back(*from);
+    while !queue.is_empty() {
+        let node = queue.pop_front().unwrap();
+        seen.insert(node);
+        for next in grid.adj(&node) {
+            f(node, next);
+            if !seen.contains(&next) {
+                queue.push_back(next);
+            }
+        }
+    }
+}
+
+/// Dijkstra's shortest path algorithm
+pub fn dijkstra<T: Debug + 'static>(
+    grid: &Grid<T>,
+    from: &Cell,
+) -> (Vec<Vec<usize>>, Map<Cell, Cell>) {
+    let (rows, cols) = grid.size();
+    let mut prev: Map<Cell, Cell> = Map::new();
+    let mut dist: Vec<Vec<usize>> = vec![vec![usize::MAX; cols]; rows];
+    dist[from.0][from.1] = 0;
+    bfs(grid, from, |node, next| {
+        let new = dist[node.0][node.1] + 1;
+        let old = dist[next.0][next.1];
+        if new < old {
+            dist[next.0][next.1] = new;
+            prev.insert(next, node);
+        }
+    });
+    (dist, prev)
 }
