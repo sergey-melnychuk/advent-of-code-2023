835ae83a9661c1db4aa444d070d1a74a7bb87e2e
sergey-melnychuk <8093171+sergey-melnychuk@users.noreply.github.com>
2023-12-18 12:39:49 +0100

    wip: day 18: 1/2

---
+++ src/bin/day19.rs
@@ -0,0 +1,22 @@
+use advent_of_code_2023::*;
+
+fn main() {
+    let _ = lines();
+
+    // println!("{}", part1()); // .
+    // println!("{}", part2()); // .
+}
+
+// cargo run --bin day19 < txt/day19.txt
+// cargo run --release --bin day19 < txt/day19.txt
+// cargo test --package advent-of-code-2023 --bin day19 -- day19 --nocapture
+
+#[cfg(test)]
+mod day19 {
+    use super::*;
+
+    #[test]
+    fn test_() {
+        //
+    }
+}
